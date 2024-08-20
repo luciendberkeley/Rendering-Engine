e85d54cc2b264ff8d651d64aa92939e52c922d50
luciendberkeley <lucienduchateau@students.berkeley.net>
2024-08-20 18:46:06 +0000

    cube rotating, fix z axis

--- static/js/render.js
+++ static/js/render.js
@@ -4,26 +4,92 @@ function DrawOBJ(object) {
 	}
 }
 
+function DrawLine(p1, p2) {
+	ctx.beginPath();
+	ctx.moveTo(p1.x, p1.y);
+	ctx.lineTo(p2.x, p2.y);
+	ctx.stroke(); 
+}
+
 function DrawCube(object) {
 	let points = [];
 	let p = object.size;
 	let m = -object.size;
-	let n = 0.1;
+	let n = 50;
 
-	points.push({ x: object.x, y: object.y, z: object.z });
-	points.push({ x: object.x + p, y: object.y, z: object.z });
-	points.push({ x: object.x, y: object.y + p, z: object.z });
-	points.push({ x: object.x + p, y: object.y + p, z: object.z });
-	points.push({ x: object.x, y: object.y, z: object.z + p });
-	points.push({ x: object.x + p, y: object.y, z: object.z + p });
-	points.push({ x: object.x, y: object.y + p, z: object.z + p });
-	points.push({ x: object.x + p, y: object.y + p, z: object.z + p });
+	points.push({ x: object.x + m, y: object.y + m, z: object.z + m});
+	points.push({ x: object.x + p, y: object.y + m, z: object.z + m});
+	points.push({ x: object.x + m, y: object.y + p, z: object.z + m});
+	points.push({ x: object.x + p, y: object.y + p, z: object.z + m});
+	points.push({ x: object.x + m, y: object.y + m, z: object.z + p});
+	points.push({ x: object.x + p, y: object.y + m, z: object.z + p});
+	points.push({ x: object.x + m, y: object.y + p, z: object.z + p});
+	points.push({ x: object.x + p, y: object.y + p, z: object.z + p});
 
+	console.log(points);
+
+	let out = [];
+	
 	for (const point of points) {
+		// Rotate
+		// X
+		let x = point.x;
+		let y = point.y * Math.cos(object.rx) - Math.sin(object.rx) * point.z;
+		let z = point.y * Math.sin(object.rx) + Math.cos(object.rx) * point.z;
+		// Y
+		x = x * Math.cos(object.ry) + Math.sin(object.ry) * z;
+		y = y;
+		z = -Math.sin(object.ry) * x + Math.cos(object.ry) * z;
+		
+		// Z
+		x = Math.cos(object.rz) * x + -Math.sin(object.rz) * y;
+		y = Math.sin(object.rz) * x + Math.cos(object.rz) * y;
+		z = z;
+
+		
+		// Perspective
+
+		/*
 		let x = (point.x * n) / point.z;
 		let y = (point.y * n) / point.z;
+		*/
 
-		ctx.fillRect(x, y, 10, 10);
-		console.log(x, y);
+		// Ortho
+		let out_x = x + 300;
+		let out_y = y + 300;
+
+		out.push({x: out_x, y: out_y});
+		
+		ctx.fillRect(out_x - 1, out_y - 1, 3, 3);
+		console.log(out_x, out_y);
 	}
+
+	// Connections:
+	/*
+	0, 1
+	0, 2
+	1, 3
+	2, 3
+
+	4, 5
+	4, 6
+	5, 7
+	6, 7
+
+	*/
+
+	DrawLine(out[0], out[1]);
+	DrawLine(out[0], out[2]);
+	DrawLine(out[1], out[3]);
+	DrawLine(out[2], out[3]);
+
+	DrawLine(out[4], out[5]);
+	DrawLine(out[4], out[6]);
+	DrawLine(out[5], out[7]);
+	DrawLine(out[6], out[7]);
+
+	DrawLine(out[0], out[4]);
+	DrawLine(out[1], out[5]);
+	DrawLine(out[2], out[6]);
+	DrawLine(out[3], out[7]);
 }
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
 let player = { x: 0, y: 0, z: 0 };
 let objects = [];
 
-objects.push({ type: "cube", x: 100, y: 0, z: 0, size: 10, color: "black" });
+objects.push({ type: "cube", x: 0, y: 0, z: 0, size: 100, rx: 0.000001, ry: 0.0000001, rz: 0, color: "black" });
 
 function Start() {
 	console.log("Start");
 }
 
 function Update() {
-	let n = 0.01;
-
+	objects[0].rx += 0.03;
+	objects[0].ry += 0.03;
 	for (const object of objects) {
 		DrawOBJ(object);
 	}
